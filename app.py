4ebf3945fa79f8b19ab1f0a67e0ee8fbcfa55e57
friedeggs <adeirff@gmail.com>
2016-03-13 17:41:47 -0400

    Database provisioning and refactoring for Flask

--- app.py
+++ app.py
@@ -11,19 +11,27 @@
 #    app.run(host='0.0.0.0', port=port)
 
 
+import os
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask.ext.sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
+ 'postgresql://localhost/techretreattemp')
+db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# db = SQLAlchemy(app)
+#  'postgresql://localhost/techretreattemp'
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     # return "Hello"
     return render_template("index.html")
 
-@app.route('/asdf', methods=['GET', 'POST'])
-def asdf():
-    # return "Hello"
-    return render_template("project_1.html")
+@app.route('/project/<id>', methods=['GET', 'POST'])
+def asdf(id):
+    # return "/project/_"+id+".html"
+    return render_template("/project/"+id+".html")
 
 if __name__ == '__main__':
     app.run()
